1aa087c59c771017404777d1a031ce3787292351
Avishka chalanamina palliyaguruge <guruge0320@gmail.com>
2026-02-08 00:26:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const panel = document.getElementById('panel');
+
+document.getElementById("convertbtn").addEventListener("click", () =>{
+    const amount = document.getElementById("amount").value;
+    const from = document.getElementById("from").value;
+    const to = document.getElementById("to").value
+
+
+    if (amount != ""){
+    
+    fetch(`https://api.exchangerate-api.com/v4/latest/${from}`)
+        .then(res => res.json())
+        .then(data => {
+            let rate = data.rates[to];
+            let result = amount * rate;
+
+            // This shows result
+            document.getElementById("result").innerText =
+            `${amount} ${from} = ${result.toFixed(2)} ${to}`;
+        })
+        .catch(() => alert("Error fetching exchange rates"));  
+    }
+
+    else{
+       alert("Please enter amount......");
+        return;
+    }
+});
